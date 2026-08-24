ac7cee8bd58d2c7b442f4b39bcb2938089e05de6
fisikaon8675 <susanto261994@gmail.com>
2026-08-25 00:18:28 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ scene.add(player);
 const fallbackPlayerMesh = new THREE.Mesh(new THREE.BoxGeometry(1, 2, 1), new THREE.MeshStandardMaterial({color: 0x0000ff}));
 player.add(fallbackPlayerMesh); 
 
-/* 
+
 // CONTOH CARA MEMASUKKAN MODEL 3D PEMAIN (Hapus tanda komentar jika punya asetnya)
 loader.load('assets/player.glb', function(gltf) {
     const model = gltf.scene;
@@ -104,7 +104,6 @@ loader.load('assets/player.glb', function(gltf) {
     player.add(model); 
     player.remove(fallbackPlayerMesh); // Hapus kotak biru saat model berhasil dimuat
 });
-*/
 
 // Fungsi Helper untuk Membuat NPC
 function createNPC(x, z, fallbackColor, modelPath = null) {
@@ -134,9 +133,9 @@ function createNPC(x, z, fallbackColor, modelPath = null) {
 }
 
 // Buat ke-3 NPC di lokasi yang berbeda di sepanjang jalan
-const npc1 = createNPC(4, -5, 0xffff00, null);  // Post 1: Kuning
-const npc2 = createNPC(-4, -15, 0x00ff00, null); // Post 2: Hijau
-const npc3 = createNPC(4, -25, 0xff0000, null);  // Post 3: Merah
+const npc1 = createNPC(4, -5, 0xffff00, npc1.glb);  // Post 1: Kuning
+const npc2 = createNPC(-4, -15, 0x00ff00, npc2.glb); // Post 2: Hijau
+const npc3 = createNPC(4, -25, 0xff0000, npc3.glb);  // Post 3: Merah
 
 // --- GENERATOR UTAMA (Garis Finish) ---
 const genGeo = new THREE.CylinderGeometry(2, 2, 4, 32); 
